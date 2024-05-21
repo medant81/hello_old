2cc4d942b93e7325a88d803b155fbcf6c2183a9b
medant81 <medant81@gmail.com>
2024-05-21 19:35:48 +0700

    Hello old version

---
+++ go.mod
@@ -0,0 +1,5 @@
+module hello_old
+
+go 1.22.1
+
+require github.com/medant81/hello v1.0.0 // indirect
---
+++ hello_old.go
@@ -0,0 +1,7 @@
+package hello_old
+
+import "github.com/medant81/hello"
+
+func Hello(){
+	hello.Hello()
+}
